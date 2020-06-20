e30b24237f29f428e0f1a94703e5c10bcd753b54
Aman Dwivedi <45517618+cherry0697@users.noreply.github.com>
2020-06-20 17:37:12 +0530

    Create String_Hash.cpp

---
+++ String_Hash.cpp
@@ -0,0 +1,14 @@
+ll compute_hash(string const &s){
+   const int p=31;
+   const int m=1e9+9;
+   
+   ll hash_value=0;
+   ll p_pow=1;
+   
+   for(char c: s){
+      hash_value=(hash_value+(c-'a'+1)*p_pow)%m;
+      p_pow=(p_pow*p)%m;
+   }
+   
+   return hash_value;
+}
